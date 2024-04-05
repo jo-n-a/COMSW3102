c94528b6af5cd9d2bdece6a8229e5674bcc26488
jo-n-a <123127394+jo-n-a@users.noreply.github.com>
2024-04-05 15:30:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("https://randomuser.me/api/?results=5").then((data)=>{
+    return data.json()
+}).then((objectData)=>{
+    console.log(objectData.results)
+    let tableData="";
+    objectData.results.map((values)=>{
+        tableData+=`<tr>
+            <td>${values.name.first}</td>
+            <td>${values.location.city}</td>
+        </tr>`;
+        
+    });
+    document.getElementById("table_body").
+    innerHTML=tableData;
+});
+//Get name.first
+//Get location.city
